3124413c36227b8b841cc80bca4acd84e6c51c5c
Your Name <you@example.com>
2023-01-26 13:25:19 +0000

    with version version/5.1

--- copy.sh
+++ copy.sh
@@ -1,13 +1,15 @@
-sh "scp ./requirements.txt  ubuntu@18.170.27.109:/home/ubuntu"
-sh "scp ./mongo-db.dockerfile  ubuntu@18.170.27.109:/home/ubuntu"
-sh "scp ./nginx.dockerfile  ubuntu@18.170.27.109:/home/ubuntu"
-sh "scp ./docker-compose-prod.yaml  ubuntu@18.170.27.109:/home/ubuntu"
-sh "scp ./init-db.js ubuntu@18.170.27.109:/home/ubuntu"
-sh "scp ./app.py ubuntu@18.170.27.109:/home/ubuntu"
-sh "scp ./test/unit-test.sh ubuntu@18.170.27.109:/home/ubuntu"
-sh "scp ./nginx/nginx.conf ubuntu@18.170.27.109:/home/ubuntu"
-sh "scp ./templates/home.html ubuntu@18.170.27.109:/home/ubuntu"
-sh "scp ./deploy.sh ubuntu@18.170.27.109:/home/ubuntu"
-sh "ssh ./ubuntu@18.170.27.109:/home/ubuntu"
+#!/bin/bash
+
+ scp ./requirements.txt  ubuntu@18.170.27.109:/home/ubuntu
+ scp ./mongo-db.dockerfile  ubuntu@18.170.27.109:/home/ubuntu
+ scp ./nginx.dockerfile  ubuntu@18.170.27.109:/home/ubuntu
+ scp ./docker-compose-prod.yaml  ubuntu@18.170.27.109:/home/ubuntu
+ scp ./init-db.js ubuntu@18.170.27.109:/home/ubuntu
+ scp ./app.py ubuntu@18.170.27.109:/home/ubuntu
+ scp ./test/unit-test.sh ubuntu@18.170.27.109:/home/ubuntu
+ scp ./nginx/nginx.conf ubuntu@18.170.27.109:/home/ubuntu
+ scp ./templates/home.html ubuntu@18.170.27.109:/home/ubuntu
+ scp ./deploy.sh ubuntu@18.170.27.109:/home/ubuntu
+ ssh ./ubuntu@18.170.27.109:/home/ubuntu
 
 
