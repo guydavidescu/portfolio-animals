1cc8dc862be6aa916f5563ef908ddc493229f75a
Your Name <you@example.com>
2023-01-26 13:27:45 +0000

    with version version/5.1

--- copy.sh
+++ copy.sh
@@ -4,9 +4,9 @@
  scp ./mongo-db.dockerfile  ubuntu@18.170.27.109:/home/ubuntu
  scp ./nginx.dockerfile  ubuntu@18.170.27.109:/home/ubuntu
  scp ./docker-compose-prod.yaml  ubuntu@18.170.27.109:/home/ubuntu
- scp ./init-db.js ubuntu@18.170.27.109:/home/ubuntu
+ scp ./init-db.js/init-db.js ubuntu@18.170.27.109:/home/ubuntu
  scp ./app.py ubuntu@18.170.27.109:/home/ubuntu
- scp ./test/unit-test.sh ubuntu@18.170.27.109:/home/ubuntu
+ scp ./test/test.sh ubuntu@18.170.27.109:/home/ubuntu
  scp ./nginx/nginx.conf ubuntu@18.170.27.109:/home/ubuntu
  scp ./templates/home.html ubuntu@18.170.27.109:/home/ubuntu
  scp ./deploy.sh ubuntu@18.170.27.109:/home/ubuntu
