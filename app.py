b36ea7395dbfb906da3feb0eb289507cd3129409
Guy Davidescu <guydavidescu@guys-mbp.home>
2023-03-09 18:09:22 +0200

    app changes

--- app.py
+++ app.py
@@ -12,7 +12,6 @@ from prometheus_flask_exporter import PrometheusMetrics
 app = Flask(__name__)
 metrics= PrometheusMetrics(app, group_by='endpoint')
 
-
 def get_db():
     client = MongoClient(host='test_mongodb',
                          port=27017, 
